9b9ba370ea58017ba850e8c27798fac3d858a904
Ruben Wouters <rubenwouters404@hotmail.com>
2015-02-21 13:24:12 +0100

    Button issue solved (on new screen)

--- project/Blu-ray_Disc_Java_Project_RUBEN_WOUTERS_DRIES_VAN_SCHEVENSTEEN/src/hellotvxlet/HelloTVXlet.java
+++ project/Blu-ray_Disc_Java_Project_RUBEN_WOUTERS_DRIES_VAN_SCHEVENSTEEN/src/hellotvxlet/HelloTVXlet.java
@@ -1,6 +1,5 @@
 package hellotvxlet;
 
-import java.awt.Color;
 import java.awt.Toolkit;
 import javax.tv.xlet.*;
 import org.havi.ui.*;
@@ -10,8 +9,8 @@ import java.awt.event.*;
 public class HelloTVXlet implements Xlet, HActionListener {
     
     // SCENE & BACKGROUND
-    private MijnComponent scene,
-                          bgImage;
+    private MijnComponent bgImage;
+    private HScene scene;
     
     // ALLE BUTTONS
     private HGraphicButton btnPlay =new HGraphicButton();
@@ -28,8 +27,9 @@ public class HelloTVXlet implements Xlet, HActionListener {
     }
 
     public void initXlet(XletContext context) {
-        // SCENE AANMAKEN
-        HScene scene = HSceneFactory.getInstance().getDefaultHScene();
+        
+        // SCENE AANMAKEN 
+        scene = HSceneFactory.getInstance().getDefaultHScene();
         
         // ALLE BUTTONS AANMAKEN
         btnPlay.setBounds(290,430,164,83);
@@ -50,50 +50,50 @@ public class HelloTVXlet implements Xlet, HActionListener {
         img5.setBounds(560,250,138,139);
         img5.setGraphicContent(Toolkit.getDefaultToolkit().getImage("Intro_illu_5.png"),128);
         
-      //ACHTERGROND
-      bgImage = new MijnComponent("Intro_bg_test.png", 0, -35);
+        //ACHTERGROND
+         bgImage = new MijnComponent("Intro_bg_test.png", 0, -35);
         
-      //RICHTING INPUT BUTTONS
-      img1.setFocusTraversal( null, btnPlay, img5, img2);
-      img2.setFocusTraversal(null, btnPlay, img1, img3);
-      img3.setFocusTraversal(null, btnPlay, img2, img4);
-      img4.setFocusTraversal(null, btnPlay, img3, img5);
-      img5.setFocusTraversal(null, btnPlay, img4, img1);
-      btnPlay.setFocusTraversal(img1, img1, null, null);
-      
-      //ADD EVERYTHING TO SCENE
-      scene.add(img1);
-      scene.add(img2);
-      scene.add(img3);
-      scene.add(img4);
-      scene.add(img5);
-      scene.add(btnPlay);
-      scene.add(bgImage);
-      
-      //ACTION NAMES MEEGEVEN
-      btnPlay.setActionCommand("btnPlay_actioned");
-      btnPlay.addHActionListener(this);
-      btnPlay.requestFocus();
-      
-      img1.setActionCommand("image1_actioned");
-      img1.addHActionListener(this);
-      img2.setActionCommand("image2_actioned");
-      img2.addHActionListener(this);
-      img3.setActionCommand("image3_actioned");
-      img3.addHActionListener(this);
-      img4.setActionCommand("image4_actioned");
-      img4.addHActionListener(this);
-      img5.setActionCommand("image5_actioned");
-      img5.addHActionListener(this);
-      
-      scene.validate();
-      scene.setVisible(true);
-      btnPlay.setVisible(true);
-      img1.setVisible(true);
-      img2.setVisible(true);
-      img3.setVisible(true);
-      img4.setVisible(true);
-      img5.setVisible(true);
+          //RICHTING INPUT BUTTONS
+          img1.setFocusTraversal( null, btnPlay, img5, img2);
+          img2.setFocusTraversal(null, btnPlay, img1, img3);
+          img3.setFocusTraversal(null, btnPlay, img2, img4);
+          img4.setFocusTraversal(null, btnPlay, img3, img5);
+          img5.setFocusTraversal(null, btnPlay, img4, img1);
+          btnPlay.setFocusTraversal(img3, img3, null, null);
+
+          //ADD EVERYTHING TO SCENE
+          scene.add(img1);
+          scene.add(img2);
+          scene.add(img3);
+          scene.add(img4);
+          scene.add(img5);
+          scene.add(btnPlay);
+          scene.add(bgImage);
+
+          //ACTION NAMES MEEGEVEN
+          btnPlay.setActionCommand("btnPlay_actioned");
+          btnPlay.addHActionListener(this);
+          btnPlay.requestFocus();
+
+          img1.setActionCommand("image1_actioned");
+          img1.addHActionListener(this);
+          img2.setActionCommand("image2_actioned");
+          img2.addHActionListener(this);
+          img3.setActionCommand("image3_actioned");
+          img3.addHActionListener(this);
+          img4.setActionCommand("image4_actioned");
+          img4.addHActionListener(this);
+          img5.setActionCommand("image5_actioned");
+          img5.addHActionListener(this);
+
+          scene.validate();
+          scene.setVisible(true);
+          btnPlay.setVisible(true);
+          img1.setVisible(true);
+          img2.setVisible(true);
+          img3.setVisible(true);
+          img4.setVisible(true);
+          img5.setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e)
@@ -128,7 +128,6 @@ public class HelloTVXlet implements Xlet, HActionListener {
             chosenImg = "Intro_illu_5.png";
             System.out.println("CHOSEN IMAGE 5");
         }
-//        System.out.println(chosenImg);
     }
     public void startXlet() {
        
@@ -143,16 +142,17 @@ public class HelloTVXlet implements Xlet, HActionListener {
     }
 
     private void StartGame() {
-       System.out.println("Game started");
-       System.out.println(chosenImg);
+        System.out.println("Game started");
+        System.out.println(chosenImg);
        
-        HScene bgScene = HSceneFactory.getInstance().getDefaultHScene();
+        // ALLES VAN SCENE HALEN (MENU)
+        scene.removeAll();
 
+        // ACHTERGROND AANMAKEN
         MijnComponent GameScene = new MijnComponent("Play_mockup.png", 0, -15);
-        bgScene.add(GameScene);
-        bgScene.validate();
-        bgScene.popToFront(GameScene);
-        bgScene.setVisible(true);
-        bgScene.repaint();
+        scene.add(GameScene);
+        scene.validate();
+        scene.setVisible(true);
+        scene.repaint();
     }
 }
