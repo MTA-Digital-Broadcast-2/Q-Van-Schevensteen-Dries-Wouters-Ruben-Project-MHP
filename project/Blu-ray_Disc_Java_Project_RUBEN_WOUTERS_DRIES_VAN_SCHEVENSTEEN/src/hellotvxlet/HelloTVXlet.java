f756fccf2f2272cd7db2aa36e4493936d28522a2
DriesVS <van.schevensteen.dries@gmail.com>
2015-05-12 02:22:43 +0200

    Added ability to use arrow to move between tiles

--- project/Blu-ray_Disc_Java_Project_RUBEN_WOUTERS_DRIES_VAN_SCHEVENSTEEN/src/hellotvxlet/HelloTVXlet.java
+++ project/Blu-ray_Disc_Java_Project_RUBEN_WOUTERS_DRIES_VAN_SCHEVENSTEEN/src/hellotvxlet/HelloTVXlet.java
@@ -11,14 +11,17 @@ public class HelloTVXlet implements Xlet, HActionListener {
     
     private HScene scene;
    
-    private MijnComponent background, imgBlock;
+    private MijnComponent background;
+
+    private HGraphicButton backButton, restartButton;
+    private HGraphicButton[] tiles;
 
     private HGraphicButton[] smallIllustrationButtons = new HGraphicButton[5];
     private String[] smallIllustrationPaths = {"Intro_illu_1.png", "Intro_illu_2.png", "Intro_illu_3.png", "Intro_illu_4.png", "Intro_illu_5.png"};
     private String chosenIllustrationPath = smallIllustrationPaths[0];
     
     public HelloTVXlet() {
-        
+        tiles = new HGraphicButton[16];
     }
 
     public void initXlet(XletContext context) {
@@ -124,23 +127,7 @@ public class HelloTVXlet implements Xlet, HActionListener {
         
         showPuzzle(chosenIllustrationPath); 
 
-        HGraphicButton backButton = new HGraphicButton(Toolkit.getDefaultToolkit().getImage("btn_back.png"));
-        backButton.setBordersEnabled(false);
-        backButton.setBounds(210, 480, 164, 83);
-        backButton.setActionCommand("backButton");
-        backButton.addHActionListener(this);
-        scene.add(backButton);
-        backButton.requestFocus();
-        
-        HGraphicButton restartButton = new HGraphicButton(Toolkit.getDefaultToolkit().getImage("btn_restart.png"));
-        restartButton.setBordersEnabled(false);
-        restartButton.setBounds(370, 480, 164, 83);
-        restartButton.setActionCommand("restartButton");
-        restartButton.addHActionListener(this);
-        scene.add(restartButton);
-        
-        backButton.setFocusTraversal(null, null, null, restartButton);
-        restartButton.setFocusTraversal(null, null, backButton, null);
+        addBackAndRestartButtons();
         
         background = new MijnComponent("Play.png", 0, -35);
         scene.add(background);
@@ -197,11 +184,14 @@ public class HelloTVXlet implements Xlet, HActionListener {
                 if(tileIndex < 10) counter = '0' + Integer.toString(tileIndex);
                 
                 currentField[i][j][0] = counter;
-                currentField[i][j][1] = "/puzzle_images/" + "puzzle" + puzzleNr + "/puzzle" + puzzleNr + "_" + counter + ".png"; 
+                currentField[i][j][1] = "/puzzle_images/" + "puzzle" + puzzleNr + "/puzzle" + puzzleNr + "_" + counter + ".png";
 
-                System.out.println(currentField[i][j][0]);
-                System.out.println(currentField[i][j][1]);
-                System.out.println("--");
+                if(tileIndex == 16)
+                    currentField[i][j][1] = "";
+
+                // System.out.println(currentField[i][j][0]);
+                // System.out.println(currentField[i][j][1]);
+                // System.out.println("--");
                 
             }
         }
@@ -226,31 +216,72 @@ public class HelloTVXlet implements Xlet, HActionListener {
 
     private void drawCurrentField() {
         int xStart = 250,
-            xAndYOffset = 59,
+            widthAndHeight = 59,
             x = xStart,
             y = 150;
+
+        int tileIndex = 0;
         
         for(int i = 0; i < currentField.length; i++) {
             for(int j = 0; j < currentField[i].length; j++) {
 
-                // HGraphicButton backButton = new HGraphicButton(Toolkit.getDefaultToolkit().getImage("btn_back.png"));
-                // backButton.setBordersEnabled(false);
-                // backButton.setBounds(210, 480, 164, 83);
-                // backButton.setActionCommand("backButton");
-                // backButton.addHActionListener(this);
-                // scene.add(backButton);
+                tiles[tileIndex] = new HGraphicButton(Toolkit.getDefaultToolkit().getImage(currentField[i][j][1]));
+                tiles[tileIndex].setBordersEnabled(false);
+                tiles[tileIndex].setBounds(x, y, widthAndHeight, widthAndHeight);
+                tiles[tileIndex].setActionCommand(currentField[i][j][0]);
+                tiles[tileIndex].addHActionListener(this);
+                scene.add(tiles[tileIndex]);
 
-                imgBlock = new MijnComponent(currentField[i][j][1], x, y);
-                scene.add(imgBlock);
+                x += widthAndHeight;
 
-                x += xAndYOffset;
+                tileIndex ++;
             }
 
             x = xStart;
-            y += xAndYOffset;
+            y += widthAndHeight;
         }
 
+        addBackAndRestartButtons();
+
+        // UP, DOWN, LEFT, RIGHT
+        tiles[0].setFocusTraversal(restartButton, tiles[4], tiles[3], tiles[1]);
+        tiles[1].setFocusTraversal(restartButton, tiles[5], tiles[0], tiles[2]);
+        tiles[2].setFocusTraversal(restartButton, tiles[6], tiles[1], tiles[3]);
+        tiles[3].setFocusTraversal(restartButton, tiles[7], tiles[2], tiles[0]);
+        tiles[4].setFocusTraversal(tiles[0], tiles[8], tiles[7], tiles[5]);
+        tiles[5].setFocusTraversal(tiles[1], tiles[9], tiles[4], tiles[6]);
+        tiles[6].setFocusTraversal(tiles[2], tiles[10], tiles[5], tiles[7]);
+        tiles[7].setFocusTraversal(tiles[3], tiles[11], tiles[6], tiles[4]);
+        tiles[8].setFocusTraversal(tiles[4], tiles[12], tiles[11], tiles[9]);
+        tiles[9].setFocusTraversal(tiles[5], tiles[13], tiles[8], tiles[10]);
+        tiles[10].setFocusTraversal(tiles[6], tiles[14], tiles[9], tiles[11]);
+        tiles[11].setFocusTraversal(tiles[7], tiles[15], tiles[10], tiles[8]);
+        tiles[12].setFocusTraversal(tiles[8], restartButton, tiles[15], tiles[13]);
+        tiles[13].setFocusTraversal(tiles[9], restartButton, tiles[12], tiles[14]);
+        tiles[14].setFocusTraversal(tiles[10], restartButton, tiles[13], tiles[15]);
+        tiles[15].setFocusTraversal(tiles[11], restartButton, tiles[14], tiles[12]);
+
         scene.repaint();
+    }
+
+    private void addBackAndRestartButtons() {
+        backButton = new HGraphicButton(Toolkit.getDefaultToolkit().getImage("btn_back.png"));
+        backButton.setBordersEnabled(false);
+        backButton.setBounds(210, 480, 164, 83);
+        backButton.setActionCommand("backButton");
+        backButton.addHActionListener(this);
+        scene.add(backButton);
+        backButton.requestFocus();
+        
+        restartButton = new HGraphicButton(Toolkit.getDefaultToolkit().getImage("btn_restart.png"));
+        restartButton.setBordersEnabled(false);
+        restartButton.setBounds(370, 480, 164, 83);
+        restartButton.setActionCommand("restartButton");
+        restartButton.addHActionListener(this);
+        scene.add(restartButton);
+        
+        backButton.setFocusTraversal(tiles[0], tiles[0], null, restartButton);
+        restartButton.setFocusTraversal(tiles[0], tiles[0], backButton, null);
     }
 }
 
